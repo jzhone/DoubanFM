a2809a71c4966293fce1abf145a6263214e476f8
jzhone <jzhone@gmail.com>
2014-08-08 12:47:01 +0800

    Initial Commit

---
+++ DoubanFM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoubanFM
+//
+//  Created by jzhone on 14-8-8.
+//  Copyright (c) 2014年 jzhone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
